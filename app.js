418ad58799434213aeaf6b80751ae776721742c8
Dylan Nirvana <dylan@dylannirvana.com>
2021-10-29 15:14:43 -0400

    adds alpha avif

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var wwwhisper = require('connect-wwwhisper');
+// var wwwhisper = require('connect-wwwhisper');
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(wwwhisper());
+// app.use(wwwhisper());
 
 app.use(express.static('client/public'))
 
